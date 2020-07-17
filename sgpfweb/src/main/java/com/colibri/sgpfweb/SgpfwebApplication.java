178e09270276e04bd0402e387260fc44a2e02286
vincentgmd <vincentgmd@users.noreply.github.com>
2020-07-16 22:11:12 -0300

    init

---
+++ sgpfweb/src/main/java/com/colibri/sgpfweb/SgpfwebApplication.java
@@ -0,0 +1,13 @@
+package com.colibri.sgpfweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgpfwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgpfwebApplication.class, args);
+	}
+
+}
---
+++ sgpfweb/src/test/java/com/colibri/sgpfweb/SgpfwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colibri.sgpfweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgpfwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
